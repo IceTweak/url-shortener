3ce8c63298a457e08c8ccb961aaa4a2fe01c867a
Roman Savoshchik <tweakice@gmail.com>
2024-10-04 00:04:54 +0600

    Создан кастомный middleware для логирования запросов

---
+++ internal/http-server/middleware/logger/logger.go
@@ -0,0 +1,47 @@
+package logger
+
+import (
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func New(log *slog.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		log = log.With(
+			slog.String("component", "middleware/logger"),
+		)
+
+		log.Info("logger middleware enabled")
+
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			// Executed upon receipt of request
+			entry := log.With(
+				slog.String("method", r.Method),
+				slog.String("path", r.URL.Path),
+				slog.String("remote_addr", r.RemoteAddr),
+				slog.String("user_agent", r.UserAgent()),
+				slog.String("request_id", middleware.GetReqID(r.Context())),
+			)
+
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+			t1 := time.Now()
+			// After all handlers (middlewares) are completed this block will be executed
+			defer func() {
+				entry.Info("request completed",
+					slog.Int("status", ww.Status()),
+					slog.Int("bytes", ww.BytesWritten()),
+					slog.String("status", time.Since(t1).String()),
+				)
+			}()
+
+			// Passes the queue to the next handler (middleware) in chain
+			next.ServeHTTP(ww, r)
+		}
+
+		return http.HandlerFunc(fn)
+	}
+}
